971cd9d9aa5d677606d33eb0b9261723fd23bb7f
Steven Broad <sebroad@yahoo.com>
2017-01-24 10:40:57 -0800

    Updated test script for ArrayStack interface

--- CSCI120/MyClass.cs
+++ CSCI120/MyClass.cs
@@ -4,15 +4,25 @@ namespace CSCI120
 {
 	public interface IOperationCounter
 	{
-		int GetOperations();
+		// Allow access to the number of operations required by 
+		// the Data Structure implementation
+		int Operations { get; }
+
+		// Reset the counter on the number of operations.
 		void ResetOperations();
 	}
 
 	namespace Untyped
 	{
 		
-		public interface IList
+		public interface IList : IOperationCounter
 		{
+			// The length of the list
+			int Length { get; }
+
+			// Initialize a blank list
+			void Initialize();
+
 			// Add an item x at position i
 			void Add (int i, object x);
 
@@ -24,21 +34,27 @@ namespace CSCI120
 
 			// Set the value x at position i
 			void Set(int i, object x);
+
+			// Clear the list
+			void Clear();
+
+			// Resize the list to capacity n
+			void Resize(int n);
 		}
 
-		interface IStack
+		public interface IStack : IOperationCounter
 		{
 			// Add an item to the top of the stack
 			void Push(object x);
 
 			// View the top item
-			object Top();
+			object Top { get; }
 
 			// Remove the top item
 			void Pop();
 		}
 
-		interface IQueue
+		public interface IQueue : IOperationCounter
 		{
 			// Add an item to the end of the queue
 			void Enqueue(object x);
@@ -46,6 +62,24 @@ namespace CSCI120
 			// Remove the item at the beginning of the queue
 			object Dequeue();
 		}
+
+		public interface IDeque : IOperationCounter
+		{
+			
+			// Add an item to the end
+			void AddLast(object x);
+
+			// Add an item to the beginning
+			void AddFirst(object x);
+
+			// Remove an item from the end
+			object RemoveLast();
+
+			// Remove an item from the beginning
+			object RemoveFirst();
+
+		}
+
 	}
 }
 
--- CSCI120_3/Program.cs
+++ CSCI120_3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CSCI120.Untyped;
 
 namespace CSCI120_3
 {
@@ -6,37 +7,119 @@ namespace CSCI120_3
 	/***
 	 * Define the ArrayStack class in namespace CSCI120_3,
 	 * implementing the interfaces 
-	 *  + CSCI120.IOperationCounter
 	 *  + CSCI120.Untyped.IList
 	 * 
-	 * using CSCI120;
+	 * This will also implement the inherited interface
+	 * IOperationCounter. Whenever your list examines or
+	 * changes the value of a position in the array, your 
+	 * operation counter variable must increase by one. 
+	 * Resetting the counter assigns the counter the value 0.
+	 * 
 	 * using CSCI120.Untyped;
 	 * 
-	 * public ArrayStack : IOperationCounter, IList
-	 * { ... }
+	 * public ArrayStack : IList
+	 * {
+	 * 	  private int counter = 0; // the operation counter
+	 *    ...
+	 * }
 	 **/
 	class Lab3
 	{
 		public static void Main (string[] args)
 		{
+			// create an object
 			int score = 0;
 			int total = 0;
 
-			// create an object
-			CSCI120.Untyped.IList arrayStack = (CSCI120.Untyped.IList)(Activator.CreateInstanceFrom ("CSCI120_3.exe", "CSCI120_3.ArrayStack").Unwrap());
-			TestStatement (arrayStack != null, "Created an arrayStack class", ref score, ref total);
+			try
+			{
+				RunTests(ref score, ref total);
+			}
+			catch (Exception ex) {
+				TestStatement (false, ex.Message + "\n" + ex.StackTrace, ref score, ref total);
+			}
 
-			/*
-			// create an array
-			object[] myArray = arrayLab.MakeArray(20);
-			TestStatement (myArray.Length == 20, "Created an array of length 20", ref score, ref total);
-			TestStatement (arrayLab.Count (myArray) == 0, "Array is all null", ref score, ref total);
-			TestStatement (arrayLab.MakeString (myArray).Length == 0, "MakeString is empty", ref score, ref total);
-			*/
 
 			Console.Write ("Press a key to exit...");
-
 			Console.ReadKey ();
+		}
+
+		public static void RunTests(ref int score, ref int total)
+		{
+
+			Console.WriteLine ("**************************************************");
+			Console.WriteLine ("\tTesting ArrayStack IList implementation");
+			Console.WriteLine ("**************************************************");
+
+			CSCI120.Untyped.IList arrayStack = null;
+			arrayStack = (CSCI120.Untyped.IList)(Activator.CreateInstanceFrom ("CSCI120_3.exe", "CSCI120_3.ArrayStack").Unwrap());
+			TestStatement (arrayStack != null, "Created an arrayStack class", ref score, ref total);
+
+			// create an array
+			arrayStack.Initialize();
+			TestStatement (arrayStack.Length == 0, "Created a new array", ref score, ref total);
+			TestStatement (arrayStack.Operations == 0, "So far, no operations", ref score, ref total);
+
+			arrayStack.Add (0, 0);
+			arrayStack.Add (1, 1);
+			arrayStack.Add (2, 2);
+			arrayStack.Add (3, 3);
+
+			TestStatement (arrayStack.Length == 4, "Array has four items", ref score, ref total);
+			TestStatement (arrayStack.Operations == 4, "Used four operations", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(0)) == 0, "Zero in first position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(1)) == 1, "One in second position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(2)) == 2, "Two in third position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(3)) == 3, "Three in fourth position", ref score, ref total);
+			TestStatement (arrayStack.Operations == 8, "Used eight operations", ref score, ref total);
+
+			arrayStack.Set (0, 5);
+			TestStatement (Convert.ToInt32(arrayStack.Get (0)) == 5, "Five in first position", ref score, ref total);
+			TestStatement (arrayStack.Operations == 10, "Used ten operations", ref score, ref total);
+
+			arrayStack.ResetOperations ();
+			TestStatement (arrayStack.Operations == 0, "Reset operations", ref score, ref total);
+			arrayStack.Resize (20);
+			TestStatement (arrayStack.Length == 4, "Array has four items", ref score, ref total);
+			TestStatement (arrayStack.Operations == 4, "Used four operations", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(0)) == 5, "Five in first position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(1)) == 1, "One in second position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(2)) == 2, "Two in third position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(3)) == 3, "Three in fourth position", ref score, ref total);
+			TestStatement (arrayStack.Operations == 8, "Used eight operations", ref score, ref total);
+
+			arrayStack.Set (4, 6);
+			TestStatement (arrayStack.Length == 4, "Array has four items", ref score, ref total);
+			TestStatement (arrayStack.Operations == 9, "Used nine operations", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(0)) == 5, "Five in first position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(1)) == 1, "One in second position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(2)) == 2, "Two in third position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(3)) == 3, "Three in fourth position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(4)) == 6, "Six in fictious fifth position", ref score, ref total);
+			TestStatement (arrayStack.Operations == 14, "Used fourteen operations", ref score, ref total);
+
+			arrayStack.Resize (21);
+			TestStatement (arrayStack.Get(4) == null, "null in fictious fifth position", ref score, ref total);
+			arrayStack.Remove (0);
+			arrayStack.Remove (1);
+			TestStatement (arrayStack.Length == 2, "Array has two items", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(0)) == 1, "One in first position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(1)) == 3, "Three in second position", ref score, ref total);
+
+			arrayStack.Clear ();
+			arrayStack.ResetOperations ();
+			TestStatement (arrayStack.Length == 0, "Array was cleared", ref score, ref total);
+			arrayStack.Add (0, 0);
+			arrayStack.Add (0, 1);
+			arrayStack.Add (0, 2);
+			arrayStack.Add (0, 3);
+			TestStatement (arrayStack.Length == 4, "Array has four items", ref score, ref total);
+			TestStatement (arrayStack.Operations == 10, "Used ten operations", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(0)) == 3, "Three in first position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(1)) == 2, "Two in second position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(2)) == 1, "One in third position", ref score, ref total);
+			TestStatement (Convert.ToInt32(arrayStack.Get(3)) == 0, "Zero in fourth position", ref score, ref total);
+
 		}
 
 		public static void TestStatement(bool value, string item, ref int score, ref int total)
