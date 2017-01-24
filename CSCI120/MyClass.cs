97933fe556e8cbce1421531b41dbe02c4001c9a8
Steven Broad <sebroad@yahoo.com>
2017-01-23 16:45:44 -0800

    Added CSCI120 and CSCI120_3 projects

---
+++ CSCI120/MyClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CSCI120
+{
+	public interface IOperationCounter
+	{
+		int GetOperations();
+		void ResetOperations();
+	}
+
+	namespace Untyped
+	{
+		
+		public interface IList
+		{
+			// Add an item x at position i
+			void Add (int i, object x);
+
+			// Remove the item at position i
+			void Remove (int i);
+
+			// Get the item at position i
+			object Get (int i);
+
+			// Set the value x at position i
+			void Set(int i, object x);
+		}
+
+		interface IStack
+		{
+			// Add an item to the top of the stack
+			void Push(object x);
+
+			// View the top item
+			object Top();
+
+			// Remove the top item
+			void Pop();
+		}
+
+		interface IQueue
+		{
+			// Add an item to the end of the queue
+			void Enqueue(object x);
+
+			// Remove the item at the beginning of the queue
+			object Dequeue();
+		}
+	}
+}
+
---
+++ CSCI120_3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CSCI120_3
+{
+
+	/***
+	 * Define the ArrayStack class in namespace CSCI120_3,
+	 * implementing the interfaces 
+	 *  + CSCI120.IOperationCounter
+	 *  + CSCI120.Untyped.IList
+	 * 
+	 * using CSCI120;
+	 * using CSCI120.Untyped;
+	 * 
+	 * public ArrayStack : IOperationCounter, IList
+	 * { ... }
+	 **/
+	class Lab3
+	{
+		public static void Main (string[] args)
+		{
+			int score = 0;
+			int total = 0;
+
+			// create an object
+			CSCI120.Untyped.IList arrayStack = (CSCI120.Untyped.IList)(Activator.CreateInstanceFrom ("CSCI120_3.exe", "CSCI120_3.ArrayStack").Unwrap());
+			TestStatement (arrayStack != null, "Created an arrayStack class", ref score, ref total);
+
+			/*
+			// create an array
+			object[] myArray = arrayLab.MakeArray(20);
+			TestStatement (myArray.Length == 20, "Created an array of length 20", ref score, ref total);
+			TestStatement (arrayLab.Count (myArray) == 0, "Array is all null", ref score, ref total);
+			TestStatement (arrayLab.MakeString (myArray).Length == 0, "MakeString is empty", ref score, ref total);
+			*/
+
+			Console.Write ("Press a key to exit...");
+
+			Console.ReadKey ();
+		}
+
+		public static void TestStatement(bool value, string item, ref int score, ref int total)
+		{
+			total ++;
+			if (value) score ++;
+			Console.WriteLine ("\t'{0}' {1} ({2}/{3})", item, value ? "Passed" : "Failed", score, total);
+		}
+	}
+}
