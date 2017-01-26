efce7bf1f1012b5ff78ab8e7178e23c0e2d562ec
John Unruh <johnumusic@gmail.com>
2017-01-25 20:40:54 -0800

    Revert "Added test layer"
    This reverts commit de96908a907fca325c13f282a36a8abf3229b4cc.

--- CSCI120_1/Program.cs
+++ CSCI120_1/Program.cs
@@ -2,95 +2,11 @@
 
 namespace CSCI120_1
 {
-	public interface IFruit
-	{
-		int Seeds { get; }
-		string Name { get; }
-		double Weight { get; }
-
-		void eat (double amount);
-		void buy (double amount);
-		void removeSeeds (int number);
-		void initialize (string Name, double Weight, int Seeds);
-
-		string ToString ();
-	}
-		
-	public interface IAnimal
-	{
-		string Species { get; }
-		int Population { get; set; }
-		IAnimal Prey { set; }
-
-		void initialize (string Species, int Population);
-		void eat (int amount);
-		string ToString ();
-	}
-
-	class Lab1
+	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			int score = 0;
-			int total = 0;
-
-			// create an object
-			IFruit fruit = (IFruit)(Activator.CreateInstanceFrom ("CSCI120_1.exe", "CSCI120_1.Fruit").Unwrap());
-			TestStatement (fruit != null, "Created a Fruit", ref score, ref total);
-
-			// initialize the object
-			fruit.initialize ("Apple", 3.0, 15);
-			TestStatement (fruit.Name == "Apple", "Fruit name is Apple", ref score, ref total);
-			TestStatement (fruit.Seeds == 15, "Fifteen seeds", ref score, ref total);
-			TestStatement (Math.Abs(fruit.Weight - 3.0) < 0.00001, "Three pounds", ref score, ref total);
-
-			// modify the object
-			fruit.eat(1.0);
-			TestStatement (Math.Abs(fruit.Weight - 2.0) < 0.00001, "Two pounds", ref score, ref total);
-			fruit.buy (1.5);
-			TestStatement (Math.Abs(fruit.Weight - 3.5) < 0.00001, "Three and a half pounds", ref score, ref total);
-			fruit.removeSeeds (12);
-			TestStatement (fruit.Seeds == 3, "Three seeds", ref score, ref total);
-
-			// object printability
-			Console.WriteLine(fruit);
-			TestStatement (fruit.ToString() == "I have 3.5 pounds of Apple with 3 seeds.", "Printable", ref score, ref total);
-
-			// create an object
-			IAnimal lion = (IAnimal)(Activator.CreateInstanceFrom ("CSCI120_1.exe", "CSCI120_1.Animal").Unwrap());
-			TestStatement (lion != null, "Created Lion", ref score, ref total);
-			IAnimal gazelle = (IAnimal)(Activator.CreateInstanceFrom ("CSCI120_1.exe", "CSCI120_1.Animal").Unwrap());
-			TestStatement (gazelle != null, "Created Gazelle", ref score, ref total);
-
-			// initialize the object
-			lion.initialize("Lion", 10);
-			TestStatement (lion.Species == "Lion", "Species name is Lion", ref score, ref total);
-			TestStatement (lion.Population == 10, "Ten lions", ref score, ref total);
-
-			gazelle.initialize ("Gazelle", 100);
-			TestStatement (gazelle.Species == "Gazelle", "Species name is Gazelle", ref score, ref total);
-			TestStatement (gazelle.Population == 100, "One hundred gazelles", ref score, ref total);
-
-			// modify the object
-			lion.Prey = gazelle;
-			lion.eat (10);
-			TestStatement (gazelle.Population == 90, "Ninety gazelles", ref score, ref total);
-
-			// object printability
-			Console.WriteLine(lion);
-			TestStatement (lion.ToString() == "There are 10 Lion.", "Printable", ref score, ref total);
-
-
-			Console.Write ("Press a key to exit...");
-
-			Console.ReadKey ();
-		}
-
-		public static void TestStatement(bool value, string item, ref int score, ref int total)
-		{
-			total ++;
-			if (value) score ++;
-			Console.WriteLine ("\t'{0}' {1} ({2}/{3})", item, value ? "Passed" : "Failed", score, total);
+			Console.WriteLine ("Hello World!");
 		}
 	}
 }
