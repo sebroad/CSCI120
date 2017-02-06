650d5193c1b545ef5854c9768e6e6f9a2a2d32b2
Steven Broad <sebroad@yahoo.com>
2017-02-06 08:12:25 -0800

    Begin the test class for linear hash table

--- CSCI120_10/Program.cs
+++ CSCI120_10/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using CSCI120;
+using CSCI120.Generic;
 
 namespace CSCI120_10
 {
-	class MainClass
+	/***
+	 * Implement the Generic IHashtable interface
+	 * in a class named LinearHashTable.
+	 * 
+	 * Also implement the IArrayBased.
+	 **/ 
+	class Lab10 : TestClass
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			(new Lab10 ()).Exec ("Implement Linear Hashtable");
 		}
 	}
 }
