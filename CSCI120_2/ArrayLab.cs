73b2ef76117253548759ec689137b815ea048f47
trialpha1 <tyler.burt@jessup.edu>
2017-02-06 18:47:23 -0800

    ArrayLab
    Stuff

---
+++ CSCI120_2/ArrayLab.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace CSCI120_2
+{
+	public class ArrayLab : IArrayLab
+	{
+		public object[] MakeArray(int length)
+		{
+			return new object[length];
+		}
+
+		public void Add(object value, object[] data)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Clear(object[] data)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Count(object[] data)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int IndexOf(object value, object[] data)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object[] MakeArray(int length)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string MakeString(object[] data)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string MakeString(object[] data, string sep)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(int index, object[] data)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+}
