b772b3d56949e59b6fe068a673e400be1a22007c
John Unruh <johnumusic@gmail.com>
2017-02-10 14:25:30 -0800

    Lab 3 work from in class 2017.02.08
    Creation of ArrayStacks.cs files and implementation of interface with the exception of the "Resize" function.
    (13/14)

---
+++ CSCI120_3/ArrayStack.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using CSCI120;
+using CSCI120.Untyped;
+
+namespace CSCI120_3
+{
+	public class ArrayStack : IList, IArrayBased
+	{
+		private int length;
+		private object[] data;
+		private int counter;
+
+		#region IArrayBased implementation
+
+		public void Initialize ()
+		{
+			length = 0;
+			counter = 0;
+			data = new object[10];
+		}
+
+		public void Resize (int n)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		#region IList implementation
+
+		public void Add (int i, object x)
+		{
+			if (length == data.Length) {
+				Resize (2 * data.Length);
+			}
+
+			// Shift to the right
+			for (int idx = length - 1; idx >= i; idx--) {
+				counter++;
+				data [idx + 1] = data [idx];
+			}
+
+			// Add in the new item
+			Set(i, x);
+
+			// Add one to lenght
+			length++;
+		}
+
+		public void Remove (int i)
+		{
+			// First shift everything to the left
+			for (int idx = i; idx < length - 1; idx++) {
+				counter++;
+				data [idx] = data [idx + 1]; // Data in idx become the data that was in the spot +1 from it
+			}
+
+			// Reduce length of the list by 1
+			length--;
+		
+			// Resize
+			if (data.Length >= 3 * length) {
+				Resize (2 * length);
+			}
+		}
+
+		public object Get (int i)			
+		{
+			counter++;
+			return data [i];
+		}
+
+		public void Set (int i, object x)
+		{
+			counter++;
+			data [i] = x;
+		}
+
+		public void Clear ()
+		{
+			length = 0;
+		}
+
+		public int Length {
+			get {
+				return length;
+			}
+		}
+
+		#endregion
+
+		#region IOperationCounter implementation
+
+		public void ResetOperations ()
+		{
+			counter = 0;
+		}
+
+		public int Operations {
+			get {
+				return counter;
+			}
+		}
+
+		#endregion
+
+		public ArrayStack ()
+		{
+			Initialize ();
+		}
+	}
+}
+
