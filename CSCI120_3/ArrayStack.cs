803fbdbc58cbe2897ab9f12ffc90b615d3bb7405
devonrudd <devon@MacBook-Pro-2.local>
2017-02-13 19:30:53 -0800

    Group A ArrayStack
    **************************************************
    	Testing ArrayStack IList implementation
    **************************************************
    	'Created an arrayStack class' Passed (1/1)
    	'Created a new array' Passed (2/2)
    	'So far, no operations' Passed (3/3)
    	'Array has four items' Passed (4/4)
    	'Used four operations' Passed (5/5)
    	'Zero in first position' Passed (6/6)
    	'One in second position' Passed (7/7)
    	'Two in third position' Passed (8/8)
    	'Three in fourth position' Passed (9/9)
    	'Used eight operations' Passed (10/10)
    	'Five in first position' Passed (11/11)
    	'Used ten operations' Passed (12/12)
    	'Reset operations' Passed (13/13)
    	'Array has four items' Passed (14/14)
    	'Used four operations' Passed (15/15)
    	'Five in first position' Passed (16/16)
    	'One in second position' Passed (17/17)
    	'Two in third position' Passed (18/18)
    	'Three in fourth position' Passed (19/19)
    	'Used eight operations' Passed (20/20)
    	'Array has four items' Passed (21/21)
    	'Used nine operations' Passed (22/22)
    	'Five in first position' Passed (23/23)
    	'One in second position' Passed (24/24)
    	'Two in third position' Passed (25/25)
    	'Three in fourth position' Passed (26/26)
    	'Six in fictious fifth position' Passed (27/27)
    	'Used fourteen operations' Passed (28/28)
    	'null in fictious fifth position' Passed (29/29)
    	'Array has two items' Passed (30/30)
    	'One in first position' Passed (31/31)
    	'Three in second position' Passed (32/32)
    	'Array was cleared' Passed (33/33)
    	'Array has four items' Passed (34/34)
    	'Used ten operations' Passed (35/35)
    	'Three in first position' Passed (36/36)
    	'Two in second position' Passed (37/37)
    	'One in third position' Passed (38/38)
    	'Zero in fourth position' Passed (39/39)
    Press a key to exit...

---
+++ CSCI120_3/ArrayStack.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using CSCI120;
+using CSCI120.Untyped;
+
+namespace CSCI120_3
+{
+	public class ArrayStack : IList, IArrayBased
+	{
+		private int length;
+		private int counter;
+		private object[] data;
+
+		public ArrayStack()
+		{
+			Initialize();
+		}
+
+		public void Initialize()
+		{
+			data = new object[10];
+			length = 0;
+			counter = 0;
+		}
+
+
+		public void Clear()
+		{
+			length = 0;
+		}
+
+		public int Length
+		{
+			get
+			{
+				return length;
+			}
+		}
+
+
+		public object Get(int i)
+		{
+			counter++;
+			return data[i];
+		}
+
+		public int Operations
+		{
+			get
+			{
+				return counter;
+			}
+		}
+
+		public void ResetOperations()
+		{
+			counter = 0;
+		}
+
+		public void Resize(int n)
+		{
+			object[] temp = new object[n];
+			for (int i = 0; i < length; i++) 
+			{
+				temp[i] = data[i];
+				counter++;
+			}
+
+			data = temp;
+		}
+
+		public void Set(int i, object x)
+		{
+			counter++;
+			data[i] = x;
+		}
+
+		public void Add(int i, object x)
+		{
+			// resize if needed
+			if (length == data.Length)
+			{
+				Resize(2 * data.Length);
+			}
+
+			// shift to the right
+			for (int idx = length - 1; idx >= i; idx--)
+			{
+				counter++;
+				data[idx + 1] = data[idx];
+			}
+
+			// add in the new item
+			Set(i, x);
+
+			// add one to Length
+			length++;
+		}
+
+		public void Remove(int i)
+		{
+			// shift everything to the left
+			for (int idx = i; idx < length - 1; idx++)
+			{
+				counter++;
+				data[idx] = data [idx + 1];
+			}
+			// reduce length of the list by 1
+			length--;
+
+			//resize
+			if (data.Length >= 3 * length)
+			{
+				Resize(length * 2);			
+			}
+		}
+	}
+}
