ca1d20b967a81ff0b3466911ff4a00b7eea70989
John Unruh <johnumusic@gmail.com>
2017-02-19 14:22:31 -0800

    Lab 4 stack class
    Implementation of stack.cs with (26/26) success on TestStatements

---
+++ CSCI120_4/Stack.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using CSCI120;
+using CSCI120.Untyped;
+
+namespace CSCI120_4
+{
+	public class Stack : IStack, IArrayBased
+
+	{
+		private int counter = 0;
+		private int top = 0;
+		private object[] data;
+
+
+		#region IArrayBased implementation
+
+		public void Initialize ()
+		{
+			top = 0;
+			counter = 0;
+			data = new object[0];
+		}
+
+		public void Resize (int n)
+		{
+			object[] temp = new object[n]; // Allocate temp array
+
+			for (int idx = 0; idx < top; idx++) {
+				counter++;
+				temp [idx] = data [idx]; // Copy array "data" to larger "temp" array
+			}
+			data = temp; // Re-assign array
+		}
+
+		#endregion
+
+		#region IStack implementation
+
+		public void Push (object x)
+		{
+			data [top] = x;
+			counter++;
+			top++;
+		}
+
+		public void Pop ()
+		{
+			counter++;
+			top--;
+		}
+
+		public object Top {
+			get {
+				if (top == 0) {
+					return null;
+				} else {
+					counter++;
+					return data [top - 1];
+				}
+
+			}
+		}
+
+		#endregion
+
+		#region IOperationCounter implementation
+
+		public void ResetOperations ()
+		{
+			counter = 0;
+		}
+
+		public int Operations {
+			get {
+				return counter;
+			}
+		}
+
+		#endregion
+
+		public Stack ()
+		{
+			Initialize ();
+		}
+	}
+}
+
