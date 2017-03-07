aeebc12910ccb7c9793e9f0d80f32c181269700f
DanielBodarev <daniel.bodarev@jessup.edu>
2017-03-06 18:46:10 -0800

    Lab 7 NONFUNCTIONAL

---
+++ CSCI120_7/LinkedStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CSCI120;
+using CSCI120.Untyped;
+
+namespace CSCI120_7 {
+	
+	public class LinkedList : IStack {
+		private Node head;
+		private int counter;
+		private int length;
+
+		public int Operations {
+			get {
+				return counter;
+			}
+		}
+
+		public object Top {
+			get {
+				if (head == null) {
+					return null;
+				} else {
+					counter++;
+					return head.Data;
+				}
+			}
+		}
+
+		public void Pop() {
+			if (head != null) {
+				head = head.Next;
+				counter++;
+				length--;
+			}
+		}
+
+		public void Push(object x) {
+			head = new Node(x, head);
+			length++;
+			counter++;
+		}
+
+		public void ResetOperations() {
+			counter = 0;
+		}
+
+		public LinkedList() {
+			head = null;
+			length = 0;
+			counter = 0;
+
+		}
+	}
+}
--- CSCI120_7/Program.cs
+++ CSCI120_7/Program.cs
@@ -4,14 +4,13 @@ using CSCI120.Untyped;
 
 namespace CSCI120_7
 {
-
-
 	/***
 	 * Implement the IStack interface in a class called LinkedList.
 	 * (No need for the IArrayBased interface this time.)
 	 * 
 	 * Utilize the CSCI120.Untyped.Node class.
 	 **/
+
 	class Lab7 : TestClass
 	{
 		public static void Main (string[] args)
