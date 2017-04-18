41441e962dfa04de5e423e514c3d56f9b7eccfcc
trialpha1 <tyler.burt@jessup.edu>
2017-04-18 16:45:25 -0700

    ArrayLab 5 addition no changes
    stuff and stuff

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -2,11 +2,88 @@
 
 namespace FinalProject
 {
-	class MainClass
+class Queue : IQueue, IArrayBased
+{
+
+	private int beginning; //where the beginning of the list is
+	private int length; //how long is the used portion of the array
+	private object temp; //temporary variable
+	private object[] data; //the array
+	private int counter;  //how many operations have we done
+
+	#region IArrayBased implementation
+
+	public void Initialize()
 	{
-		public static void Main(string[] args)
+		beginning = 0;
+		length = 0;
+		counter = 0;
+		data = new object[10];
+	}
+
+	public void Resize(int n)
+	{
+		if (n < 1)
+			return;
+		n = Math.Max(length, n);
+		object[] new_data = new object[n];
+		for (int idx = 0; idx < length; idx++)
 		{
-			Console.WriteLine("Hello World!");
+			counter++;
+			new_data[idx] = data[idx];
+		}
+
+		data = new_data;
+	}
+
+	#endregion
+
+
+	#region IQueue implementation
+
+	public void Enqueue(object x)
+	{
+		length++;
+		data[(length - 1)] = x;
+		counter++;
+	}
+
+	public object Dequeue()
+	{
+		if (length == 0)
+		{
+			return null;
+		}
+		else
+		{
+			temp = data[beginning];
+			beginning++;
+			length--;
+			counter++;
+			return temp;
 		}
 	}
+
+	#endregion
+
+	#region IOperationCounter implementation
+
+	public void ResetOperations()
+	{
+		counter = 0;
+	}
+
+	public int Operations
+	{
+		get
+		{
+			return counter;
+		}
+	}
+
+	#endregion
+
+
+
+
 }
