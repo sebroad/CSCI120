c69ed39692e4b3ea5ebb0540c386ff5012f97f4f
trialpha1 <tyler.burt@jessup.edu>
2017-04-18 16:48:03 -0700

    added tabs
    tabs for days

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -2,88 +2,84 @@
 
 namespace FinalProject
 {
-class Queue : IQueue, IArrayBased
-{
-
-	private int beginning; //where the beginning of the list is
-	private int length; //how long is the used portion of the array
-	private object temp; //temporary variable
-	private object[] data; //the array
-	private int counter;  //how many operations have we done
-
-	#region IArrayBased implementation
-
-	public void Initialize()
+	class Queue : IQueue, IArrayBased
 	{
-		beginning = 0;
-		length = 0;
-		counter = 0;
-		data = new object[10];
-	}
 
-	public void Resize(int n)
-	{
-		if (n < 1)
-			return;
-		n = Math.Max(length, n);
-		object[] new_data = new object[n];
-		for (int idx = 0; idx < length; idx++)
+		private int beginning; //where the beginning of the list is
+		private int length; //how long is the used portion of the array
+		private object temp; //temporary variable
+		private object[] data; //the array
+		private int counter;  //how many operations have we done
+
+		#region IArrayBased implementation
+
+		public void Initialize()
 		{
+			beginning = 0;
+			length = 0;
+			counter = 0;
+			data = new object[10];
+		}
+
+		public void Resize(int n)
+		{
+			if (n < 1)
+				return;
+			n = Math.Max(length, n);
+			object[] new_data = new object[n];
+			for (int idx = 0; idx < length; idx++)
+			{
 			counter++;
 			new_data[idx] = data[idx];
+			}
+
+			data = new_data;
 		}
 
-		data = new_data;
-	}
-
-	#endregion
+		#endregion
 
 
-	#region IQueue implementation
+		#region IQueue implementation
 
-	public void Enqueue(object x)
-	{
-		length++;
-		data[(length - 1)] = x;
-		counter++;
-	}
-
-	public object Dequeue()
-	{
-		if (length == 0)
+		public void Enqueue(object x)
 		{
-			return null;
-		}
-		else
-		{
-			temp = data[beginning];
-			beginning++;
-			length--;
+			length++;
+			data[(length - 1)] = x;
 			counter++;
-			return temp;
 		}
-	}
 
-	#endregion
-
-	#region IOperationCounter implementation
-
-	public void ResetOperations()
-	{
-		counter = 0;
-	}
-
-	public int Operations
-	{
-		get
+		public object Dequeue()
 		{
-			return counter;
+			if (length == 0)
+			{
+				return null;
+			}
+			else
+			{
+				temp = data[beginning];
+				beginning++;
+				length--;
+				counter++;
+				return temp;
+			}
 		}
+
+		#endregion
+
+		#region IOperationCounter implementation
+
+		public void ResetOperations()
+		{
+			counter = 0;
+		}
+
+		public int Operations
+		{
+			get
+			{
+				return counter;
+			}
+		}
+
+		#endregion
 	}
-
-	#endregion
-
-
-
-
-}
